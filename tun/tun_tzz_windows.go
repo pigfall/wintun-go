aab56720149873d8f188fab60735327f173b3619
tzz <koi_town@outlook.com>
2021-10-13 11:47:17 +0800

    Add tun tzz

---
+++ tun/tun_tzz_windows.go
@@ -0,0 +1,37 @@
+package tun
+
+import(
+	"fmt"
+	"golang.zx2c4.com/wireguard/tun/wintun"
+	"golang.org/x/sys/windows"
+)
+
+
+func NewTun(ifname string,mtu int)(Device,error){
+	wt, err := wintun.CreateAdapter("" ,ifname,nil)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating interface: %w", err)
+	}
+
+	forcedMTU := 1420
+	if mtu > 0 {
+		forcedMTU = mtu
+	}
+
+	tun := &NativeTun{
+		wt:        wt,
+		name:      ifname,
+		handle:    windows.InvalidHandle,
+		events:    make(chan Event, 10),
+		forcedMTU: forcedMTU,
+	}
+
+	tun.session, err = wt.StartSession(0x800000) // Ring capacity, 8 MiB
+	if err != nil {
+		tun.wt.Close()
+		close(tun.events)
+		return nil, fmt.Errorf("Error starting session: %w", err)
+	}
+	tun.readWait = tun.session.ReadWaitEvent()
+	return tun, nil
+}
